726dd61dbfe82a31b917821b7454868cb2d2175e
Derick <derickeduardo22@gmail.com>
2025-11-07 19:52:05 -0300

    Atualização: favicon e SEO

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// ===== Efeito de revelar seções ao rolar =====
+  const reveals = document.querySelectorAll('.reveal');
+
+  window.addEventListener('scroll', () => {
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const revealTop = reveals[i].getBoundingClientRect().top;
+      const revealPoint = 100;
+
+      if (revealTop < windowHeight - revealPoint) {
+        reveals[i].classList.add('active');
+      } else {
+        reveals[i].classList.remove('active');
+      }
+    }
+  });
+
